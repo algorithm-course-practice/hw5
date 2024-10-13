f6d1bcf8d6c54fc2517df8e1ca85ccfa5636a369
Almaz <almazgara@gmail.com>
2024-10-13 11:55:26 +0300

    HW5

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class HomeWork {
 
     /**
@@ -18,6 +25,104 @@ public class HomeWork {
     double calculate(String expr) {
 
         return 0;
+    }
+
+
+    static List<String> OPERATORS = asList("+", "-", "*", "/");
+
+
+    /**
+     * Алгоритм сортировочной станции
+     * c функциями
+     * @see <a href="https://ru.wikipedia.org/wiki/Алгоритм_сортировочной_станции">https://ru.wikipedia.org/wiki/Алгоритм_сортировочной_станции</a>
+     * @param inputString
+     * @return
+     */
+    static String translate(String inputString) {
+        String[] input = inputString.split(" ");
+        List<String> output = new ArrayList<>();
+        Deque<String> stack = new LinkedList<>();
+        for (String cur : input) {
+            //Если токен — число, то добавить его в очередь вывода.
+            if (isNumber(cur)) {
+                output.add(cur);
+                continue;
+            }
+            //Если токен — функция, то поместить его в стэк:
+            if(cur.length() > 1){
+                stack.push(cur);
+                continue;
+            }
+            //Если токен — разделитель аргументов функции(запятая):
+            if (",".equals(cur)){
+                while(!stack.isEmpty() && !"(".equals(stack.peek())){
+                    output.add(stack.pop());
+                }
+                //Если стек закончился до того, как был встречен токен открывающая скобка, то в выражении пропущена скобка.
+                if (stack.isEmpty()) {
+                    throw new IllegalArgumentException("Missing '(' in expression");
+                }
+            }
+            //Если токен — оператор op1, то:
+            if (OPERATORS.contains(cur)) {
+                //Пока присутствует на вершине стека токен оператор op2, чей приоритет выше или равен приоритету op1,
+                // и при равенстве приоритетов op1 является левоассоциативным:
+                while (!stack.isEmpty() && !"(".equals(stack.peek())  && priority(stack.peek()) >= priority(cur)) {
+                    //Переложить op2 из стека в выходную очередь;
+                    output.add(stack.pop());
+                }
+                //Положить op1 в стек.
+                stack.push(cur);
+            }
+            //Если токен — открывающая скобка, то положить его в стек.
+            if ("(".equals(cur)) {
+                stack.push(cur);
+            }
+            //Если токен — закрывающая скобка:
+            if (")".equals(cur)) {
+                //Пока токен на вершине стека не открывающая скобка
+                while (!stack.isEmpty() && !"(".equals(stack.peek())) {
+                    //Переложить оператор из стека в выходную очередь.
+                    output.add(stack.pop());
+                }
+                //Если стек закончился до того, как был встречен токен открывающая скобка, то в выражении пропущена скобка.
+                if (stack.isEmpty()) {
+                    throw new IllegalArgumentException("Missing '(' in expression");
+                }
+                //Выкинуть открывающую скобку из стека, но не добавлять в очередь вывода.
+                stack.pop();
+                //Если токен на вершине стэка функция, то переложить ее в выходную очередь:
+                if (!stack.isEmpty() && stack.peek().length() > 1){
+                    output.add(stack.pop());
+                }
+            }
+        }
+        //Если больше не осталось токенов на входе:
+        //Пока есть токены операторы в стеке:
+        while (!stack.isEmpty()) {
+            //Если токен оператор на вершине стека — открывающая скобка, то в выражении пропущена скобка.
+            if ("(".equals(stack.peek())) {
+                throw new IllegalArgumentException("Missing ')' in expression");
+            }
+            //Переложить оператор из стека в выходную очередь.
+            output.add(stack.pop().toString());
+        }
+
+
+        return String.join(" ", output);
+    }
+
+    private static boolean isNumber(String number) {
+        try{
+            Integer.valueOf(number);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    private static int priority(String operator) {
+        return ("+".equals(operator) || "-".equals(operator)) ? 1 : 2;
     }
 
 }
--- src/test/java/org/example/HomeWorkTest.java
+++ src/test/java/org/example/HomeWorkTest.java
@@ -2,7 +2,9 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.example.HomeWork.translate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class HomeWorkTest {
 
@@ -10,7 +12,25 @@ class HomeWorkTest {
 
     @Test
     void check() {
-        assertEquals(1.1, 1.101, 0.0001);
+        assertEquals("2 3 4 * +", translate("2 + 3 * 4"));
+        assertEquals("1 2 + 3 -", translate("1 + 2 - 3"));
+        assertEquals("2 3 + 4 *", translate("( ( 2 + 3 ) ) * 4"));
+        assertEquals("2 3 4 * + 5 - 7 6 * 3 / + 2 3 2 * - 5 2 - 2 * +", translate("2 + 3 * 4 - 5 + 7 * 6 / 3 - 2 * 3 2 + ( 5 - 2 ) * 2"));
+        assertThrows(IllegalArgumentException.class,() -> translate("( 2 + 3 ) ) * 4"));
+        assertThrows(IllegalArgumentException.class,() -> translate("( 2 + 3 d ) ) * 4"));
+        assertEquals("1 2 3 * -", translate("1 - 2 * 3"));
+        assertEquals("1 2 - 20 sin +", translate("1 - 2 + sin ( 20 )"));
+        assertEquals("3 1 2 - * 10 sin *", translate("3 * ( 1 - 2 ) * sin ( 10 )"));
+        assertEquals("4 1 2 * 3 - * 11 cos *", translate("4 * ( 1 * 2 - 3 ) * cos ( 11 )"));
+        assertEquals("4 1 2 * 3 - * 11 cos +", translate("4 * ( 1 * 2 - 3 ) + cos ( 11 )"));
+        assertEquals("5 sin 4 1 2 * 3 - * 11 cos + +", translate("sin ( 5 ) + ( 4 * ( 1 * 2 - 3 ) + cos ( 11 ) )"));
+        assertEquals("1 2 sqr +", translate("1 + sqr ( 2 )"));
+        assertEquals("1 2 * 5 7 pwr +", translate("1 * 2 + pwr ( 5 , 7 )"));
+        assertEquals("4 1 2 * 3 - 10 7 6 * + sqr pwr + 10 3 * + 5 cos +",
+                translate("4 + pwr ( 1 * 2 - 3 , sqr ( 10 + 7 * 6 ) ) + 10 * 3 + cos ( 5 )"));
+
+
+        assertEquals(1.1, 1.1000001, 0.0001);
     }
 
 }
